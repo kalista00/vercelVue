c17edb48c00541b9b1af804b6ad99ce62c7162db
changhee kim <ch.kim@theuber.co.kr>
2023-12-21 12:55:04 +0900

    test

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import HelloWorld from './components/HelloWorld.vue'
-
+import Practice from './components/Practice.vue'
 
 createApp(HelloWorld).mount('#app')
-createApp(HelloWorld).mount('#app1')
+createApp(Practice).mount('#app1')
